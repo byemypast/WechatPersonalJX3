3ebd40cad8a39b9de4b07bc62074127cb0f76233
wangjunyi2008 <wangjunyi2008@sina.com>
2017-03-14 16:09:04 +0800

    version 0.91 [+]增加看图猜技能功能（少林） [-]修改一处看图猜技能的BUG

--- core/fetchhm.py
+++ core/fetchhm.py
@@ -10,17 +10,24 @@ class GetHMInfo(object):
 		pass
 	def GetSkill(self,skillname):
 		try:	
+			returnlist = []
 			r = requests.get(self.url % skillname)
-			p = re.compile('<img class="wx-share" src="(.*?)" alt')
-			p2 = re.compile('<span class="font-106">(.*?)</span>')
-			p3 = re.compile('<span class="font-100">(.*?)</span>')
-			p4 = re.compile('<div class="line">(.*?)</div>')
+			p = re.compile('<img class="wx-share" src="(.*?)" alt') #icon
+			p2 = re.compile('<span class="font-106">(.*?)</span>') #距离等
+			p3 = re.compile('<span class="font-100">(.*?)</span>') #具体描述
+			p4 = re.compile('<span class="font-31">(.*?)</span>') #技能名称
 			icon = p.findall(r.text)
 			iconimg = BytesIO(requests.get(icon[0]).content)
 			iconobj = Image.open(iconimg)
 			describe_1 = p2.findall(r.text)
 			describe_2 = p3.findall(r.text)
-			return iconobj,list(zip(describe_1,describe_2))
+			skillnames = p4.findall(r.text)
+			for index,name in enumerate(skillnames):
+				if name == skillname:
+					returnlist.append((describe_1[index],describe_2[index]))
+				else:
+					print(name,skillname)
+			return iconobj,returnlist
 		except Exception as err:
 			debug("获取技能信息错误！信息："+str(err),'错误')
 			return None,None
--- core/pop3.py
+++ core/pop3.py
@@ -1,23 +1,34 @@
-import imaplib
-import email.header
-class imapgame(object):
+#-*- encoding: utf-8 -*-
+#-*- encoding: gbk -*-
+import poplib
+import base64
+class POP3Receive(object):
 	def __init__(self,host,username,pwd):
 		self.host = host
 		self.user = username
 		self.pwd = pwd
 		
-		self.imap = imaplib.IMAP4(host)
-		self.imap.login(username,pwd)
-		self.imap.select()
+		self.pop3 = poplib.POP3(host)
+		self.pop3.user(username)
+		self.pop3.pass_(pwd)
+		ret = self.pop3.stat()
+		print(ret)
+		down = self.pop3.retr(1)
+		strs = ""
+		for index,line in enumerate(down[1]):
+			if index>50:
+				strs += line.decode('gbk')
+		print(base64.b64decode(strs).decode('gbk'))
+
 	def GetFirstMail(self):
 		newestid = (self.imap.search(None,'ALL')[1][0].decode('utf-8').split(" ")[-1])
-		resp,maildata = self.imap.fetch(str(newestid),"(RFC822)")
+		resp,maildata = self.imap.fetch(str(newestid),"BODY[TEXT]")
 		mailtext = maildata[0][1]
-		msg = email.message_from_bytes(mailtext)
-		subject = email.header.decode_header(msg['Subject'])[0][0].decode('utf-8')
-		content = msg.get_payload(decode=True)
-		print(content)
+		print(mailtext)
+		#msg = email.message_from_bytes(mailtext)
+		#subject = email.header.decode_header(msg['Subject'])[0][0].decode('utf-8')
+		#content = msg.get_payload(decode=True)
+		#print(content)
 
 
-m = imapgame("imap.sina.com","jx3trader@sina.com","pkucoe2016")
-m.GetFirstMail()
+m = POP3Receive("imap.sina.com","jx3trader@sina.com","pkucoe2016")
